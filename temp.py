cb2943be5f4f63810538e5e8155ccdc889f36972
akshatamulagundamath26 <akshatamulagundamath26@gmail.com>
2025-11-11 10:49:41 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,18 +1,16 @@
 import sys
 
-# Check if the user has provided the temperature argument
+
 if len(sys.argv) != 2:
     print("Usage: python temperature_check.py <temperature_in_Celsius>")
     sys.exit(1)
 
-# Read temperature from command line
 temperature = float(sys.argv[1])
 
-# Check temperature range
 if temperature < 15:
     print("Cold")
 elif 15 <= temperature <= 30:
     print("Normal")
 else:
-    print("Hot")
+    print("Hot")
 
